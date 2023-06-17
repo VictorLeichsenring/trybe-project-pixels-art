db684d2a8b0d6c53d6012940cd18891e52c5ebd5
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-06-17 00:51:54 -0300

    Função para selecionar cores na paleta

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const pixelBoard = document.getElementById('pixel-board');
 const colorPalette = document.getElementById('color-palette');
-
-
+const colorsList = document.getElementsByClassName('color');
 
 function createPalette() {
     for (let i = 0; i < 4; i += 1) {
         let element = document.createElement('div');
         element.classList.add('color');
         colorPalette.appendChild(element);
+        element.addEventListener('click', colorSelect)
     }
 }
 
@@ -18,6 +18,16 @@ function createBoard() {
         pixelBoard.appendChild(element);
     }
 }
+
+function colorSelect(event) {
+    for (let i =0; i < colorsList.length; i += 1) {
+        let color = colorsList[i];
+        let classes = color.classList;
+        classes.remove('selected');
+    }
+    event.target.classList.add('selected');
+}
+
 
 createPalette();
 createBoard();
